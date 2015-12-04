a803a0f52a7d520edfdb568aa7fb2923397d395d
levelfour <levelfourslv@gmail.com>
2015-12-05 01:29:24 +0900

    split lines to .text/.data

--- assembler.ml
+++ assembler.ml
@@ -9,6 +9,26 @@ let bytes_of_int i =
   let c3 = Char.chr (i land 0xff) in
   List.fold_left (fun acc c -> acc ^ (String.make 1 c)) "" [c0;c1;c2;c3]
 
+(* Split input assembly lines to .text/.data section *)
+type section = Text | Data
+let split_input lines =
+  let rec split_input' sec textls datals = function
+    (* End of buffer *)
+    | [] -> (List.rev textls, List.rev datals)
+
+    (* Check current section then add line to either section *)
+    | l::ls ->
+        (let l = ExtString.String.strip l in
+         match l with
+         | "" -> split_input' sec textls datals ls
+         | ".text" -> split_input' Text textls datals ls
+         | ".data" -> split_input' Data textls datals ls
+         | _ when sec = Text -> split_input' sec (l::textls) datals ls
+         | _ when sec = Data -> split_input' sec textls (l::datals) ls
+         | _ -> assert false) in
+
+  split_input' Text [] [] lines
+
 (* Emit header *)
 let emit_header oc text_size data_size entry_point =
   Printf.fprintf oc "AQIL";
@@ -27,24 +47,25 @@ let rec emit_text oc lines addr_map =
 
 (* Executable code emitter *)
 let emit oc lines =
+  (* Ignore .extern pseudo-instruction *)
+  let lines =
+    List.filter (fun l -> not (ExtString.String.exists l ".extern")) lines in
+
   (* Split lines to .text/.data section *)
-  (* TODO: implement
   let text_lines, data_lines = split_input lines in
-  *)
 
   (* Build mapping between label name and address *)
   (* TODO: implement
-  let addr_map = build_addr_map data_lines in
+  let addr_map = build_addr_map text_lines data_lines in
   *)
 
   (* Emit Aquila header *)
-  let text_size = 0 (* List.length text_lines *) in
-  let data_size = 0 (* List.length data_lines *) in
+  let text_size = List.length text_lines in
+  let data_size = List.length data_lines in
   let entry_point = 0 (* AddrMap.find (label "entry") addr_map *) in
   emit_header oc text_size data_size entry_point;
 
   (* Emit .text section *)
-  let text_lines = lines in (* TODO: delete after implementation *)
   emit_text oc text_lines [] (* addr_map *);
 
   (* Emit .data section *)
