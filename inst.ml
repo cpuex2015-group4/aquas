63bb665ae08100b2e86741641906330c2ebb018d
levelfour <levelfourslv@gmail.com>
2015-12-05 23:43:59 +0900

    hlt / nop

--- inst.ml
+++ inst.ml
@@ -1,10 +1,15 @@
+(* X-Format *)
+let hlt () = "\x00\x00\x00\x00"
+let nop () = "\x00\x00\x00\x02"
+
+(* Assembly -> Bytecode *)
 let bytecode line addr_map =
   let line = ExtString.String.strip line in
   match Str.split (Str.regexp "[ \t,]") line with
   | [] -> assert false
   | op :: args -> begin
     match op with
-    | "ld" -> "0001"
-    | "st" -> "0002"
+    | "hlt" -> hlt ()
+    | "nop" -> nop ()
     | _ -> failwith (Printf.sprintf "invalid mnemonic `%s`" op)
   end
---
+++ sample/test.s
@@ -0,0 +1,7 @@
+.data
+.word 0x38FFFFFF
+
+.text 
+_leml_entry:
+	nop
+	hlt
